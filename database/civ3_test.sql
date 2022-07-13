ec37bb8aae1b8dbcff2b51a1ce7f34e2e65a320d
Manish Bhuva <manish.bhuvait@gmail.com>
2022-07-13 12:34:05 +0530

    database create sql file with data

--- database/civ3_test.sql
+++ database/civ3_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2022 at 07:25 AM
+-- Generation Time: Jul 13, 2022 at 09:03 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -36,6 +36,16 @@ CREATE TABLE `products` (
   `timestamp` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `sku`, `title`, `description`, `status`, `timestamp`) VALUES
+(1, 'prod1', 'prod1', 'prod1', 'Active', '2022-07-13 12:30:17'),
+(2, 'prod2', 'prod2', 'prod2', 'Active', '2022-07-13 12:30:17'),
+(3, 'prod3', 'prod3', 'prod3', 'Active', '2022-07-13 12:30:44'),
+(4, 'prod4', 'prod4', 'prod4', 'Inactive', '2022-07-13 12:30:44');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +59,16 @@ CREATE TABLE `users` (
   `status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
   `timestamp` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `status`, `timestamp`) VALUES
+(1, 'Manish', 'manish.bhuvait@gmail.com', 'Active', '2022-07-13 12:23:43'),
+(2, 'Rohan', 'rohan@test.com', 'Active', '2022-07-13 12:23:43'),
+(3, 'Hema', 'hema@test.com', 'Active', '2022-07-13 12:23:43'),
+(4, 'Vidhi', 'Vidhi@test.com', 'Active', '2022-07-13 12:23:43');
 
 -- --------------------------------------------------------
 
@@ -65,6 +85,16 @@ CREATE TABLE `user_products` (
   `status` enum('Active','Inactive') NOT NULL,
   `timestamp` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_products`
+--
+
+INSERT INTO `user_products` (`id`, `user_id`, `product_id`, `price`, `qty`, `status`, `timestamp`) VALUES
+(1, 1, 1, 100.00, 5, 'Active', '2022-07-13 12:31:46'),
+(2, 1, 2, 200.00, 4, 'Active', '2022-07-13 12:31:46'),
+(3, 2, 3, 300.00, 3, 'Active', '2022-07-13 12:32:24'),
+(4, 3, 3, 310.00, 3, 'Inactive', '2022-07-13 12:32:24');
 
 --
 -- Indexes for dumped tables
@@ -98,13 +128,19 @@ ALTER TABLE `user_products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_products`
 --
 ALTER TABLE `user_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -114,8 +150,8 @@ ALTER TABLE `user_products`
 -- Constraints for table `user_products`
 --
 ALTER TABLE `user_products`
-  ADD CONSTRAINT `user_products_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_products_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_products_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_products_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
